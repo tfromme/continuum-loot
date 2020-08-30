61e5ea93ff5be6562bb3cac22a4a1e2ccc5c3e66
Tim Fromme <tfromme44@live.com>
2020-08-30 13:36:02 -0400

    material-table

--- src/modules/Tables.js
+++ src/modules/Tables.js
@@ -1,63 +1,37 @@
 import React from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import MaterialTable from 'material-table'
 
-
-function createData(name, class_, rank, role, notes) {
-  return { name, class_, rank, role, notes };
-}
 
 export class PlayerTable extends React.Component {
   playersToRows() {
     var rows = [];
     var players = this.props.players;
     for (const player in players) {
-      rows.push(createData(
-        players[player].name,
-        players[player].class,
-        players[player].rank,
-        players[player].role,
-        players[player].notes,
-      ));
+      rows.push({
+        'name': players[player].name,
+        'class': players[player].class,
+        'rank': players[player].rank,
+        'role': players[player].role,
+        'notes': players[player].notes,
+      });
     }
     return rows;
   }
 
   render() {
-    const headers = ['Name', 'Class', 'Rank', 'Role', 'Notes'];
-    const headerItems = headers.map((header, index) =>
-      <TableCell key={index}>{header}</TableCell>
-    );
-
-    const rows = this.playersToRows().map((row) =>
-      <TableRow key={row.name}>
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell>{row.class_}</TableCell>
-        <TableCell>{row.rank}</TableCell>
-        <TableCell>{row.role}</TableCell>
-        <TableCell>{row.notes}</TableCell>
-      </TableRow>
-    );
     return (
-      <TableContainer component={Paper}>
-        <Table className="table">
-          <TableHead>
-            <TableRow>
-              {headerItems}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <MaterialTable
+        columns={[
+          { title: 'Name', field: 'name', defaultSort: 'asc' },
+          { title: 'Class', field: 'class' },
+          { title: 'Rank', field: 'rank' },
+          { title: 'Role', field: 'role' },
+          { title: 'Notes', field: 'notes' },
+        ]}
+        data={ this.playersToRows() }
+        title="Players"
+        options={ {'paging': false} }
+      />
     );
   }
 }
